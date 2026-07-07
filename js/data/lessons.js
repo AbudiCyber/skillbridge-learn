bfbc51fe9910bb4ec39c22c03f9cf5d96da3e253
AbudiCyber <a95404829@gmail.com>
2026-07-07 22:58:20 +0300

    Add lessons data placeholder

---
+++ js/data/lessons.js
@@ -0,0 +1,10 @@
+export const lessons = [
+  {
+    id: "eng-001",
+    unitId: "alphabet-basics",
+    title: "English Letters",
+    goal: "تعرف على فكرة الحروف الإنجليزية الكبيرة والصغيرة.",
+    xpReward: 20,
+    estimatedMinutes: 3
+  }
+];
