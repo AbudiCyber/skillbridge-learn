78494851ea62e6e8f43532759b59333dc72540ad
AbudiCyber <a95404829@gmail.com>
2026-07-07 22:58:13 +0300

    Add units data placeholder

---
+++ js/data/units.js
@@ -0,0 +1,16 @@
+export const units = [
+  {
+    id: "alphabet-basics",
+    trackId: "english-beginner",
+    title: "Alphabet Basics",
+    order: 1,
+    isLocked: false
+  },
+  {
+    id: "basic-greetings",
+    trackId: "english-beginner",
+    title: "Basic Greetings",
+    order: 2,
+    isLocked: true
+  }
+];
