ce20d33c4d3fa9c47963d297b005a1367bb5d4f7
AbudiCyber <a95404829@gmail.com>
2026-07-07 22:55:15 +0300

    Add app constants

---
+++ js/constants.js
@@ -0,0 +1,14 @@
+export const APP_NAME = "SkillBridge Learn";
+export const APP_VERSION = "0.1.0";
+
+export const ROUTES = {
+  HOME: "home",
+  LEARN: "learn",
+  LIBRARY: "library",
+  PROGRESS: "progress",
+  SETTINGS: "settings"
+};
+
+export const STORAGE_KEYS = {
+  USER_STATE: "skillbridge:user-state"
+};
