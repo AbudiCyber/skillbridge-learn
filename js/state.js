823e4353e0953f2a6b5fe37750bf6bf2d7c7a8c4
AbudiCyber <a95404829@gmail.com>
2026-07-08 00:52:01 +0300

    Harden default progress state

--- js/state.js
+++ js/state.js
@@ -1,9 +1,13 @@
 export const defaultState = {
   route: "home",
   activeLessonId: "eng-001",
+  uiLanguage: "ar",
   xp: 0,
   streak: 0,
   completedLessons: [],
+  completedQuizzes: [],
+  quizAnswers: {},
+  xpEvents: [],
   savedWords: [],
   selectedGoal: null
 };
