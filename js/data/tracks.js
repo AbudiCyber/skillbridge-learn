4d4c86e72325c23a1f604e7033ce82f700d3216e
AbudiCyber <a95404829@gmail.com>
2026-07-07 22:58:06 +0300

    Add tracks data placeholder

---
+++ js/data/tracks.js
@@ -0,0 +1,8 @@
+export const tracks = [
+  {
+    id: "english-beginner",
+    title: "English for Beginners",
+    description: "Short English lessons for Arabic-speaking beginners.",
+    status: "active"
+  }
+];
