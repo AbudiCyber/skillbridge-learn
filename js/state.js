ec45f7ba6397c303f1f7aeccc943489bd34c4af5
AbudiCyber <a95404829@gmail.com>
2026-07-08 10:57:26 +0300

    Add settings defaults and reset helper

--- js/state.js
+++ js/state.js
@@ -2,6 +2,7 @@ export const defaultState = {
   route: "home",
   activeLessonId: "eng-001",
   uiLanguage: "ar",
+  themeMode: "dark",
   xp: 0,
   streak: 0,
   bestStreak: 0,
@@ -12,7 +13,8 @@ export const defaultState = {
   xpEvents: [],
   savedWords: [],
   wordReviews: {},
-  selectedGoal: null
+  selectedGoal: "daily-lesson",
+  resetConfirmArmed: false
 };
 
 let appState = { ...defaultState };
@@ -28,4 +30,14 @@ export function setState(nextState) {
   };
 
   return appState;
+}
+
+export function createResetState(currentState) {
+  return {
+    ...defaultState,
+    uiLanguage: currentState.uiLanguage || defaultState.uiLanguage,
+    themeMode: currentState.themeMode || defaultState.themeMode,
+    selectedGoal: currentState.selectedGoal || defaultState.selectedGoal,
+    route: "settings"
+  };
 }
