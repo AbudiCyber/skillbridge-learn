78d523167a91b9e5da55f6044afa030a05cbdcd4
AbudiCyber <a95404829@gmail.com>
2026-08-04 13:11:29 +0300

    Release v0.3.3 achievement QA coverage

--- js/config/appMeta.js
+++ js/config/appMeta.js
@@ -1,4 +1,4 @@
 export const APP_NAME = "SkillBridge Learn";
-export const APP_VERSION = "0.3.2";
+export const APP_VERSION = "0.3.3";
 export const CACHE_VERSION = `v${APP_VERSION}`;
 export const CACHE_PREFIX = "skillbridge-learn";
